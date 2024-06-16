753e4a44163f09154748cb374be3e465171e15f9
EwanMcA <ewan.m@hotmail.com>
2024-06-16 17:04:32 +0100

    make fun little wave better

--- src/simulation.ts
+++ src/simulation.ts
@@ -3,9 +3,8 @@ import { SimulationState } from "./types";
 const simulate = (simState: SimulationState) => {
   for (let i = 0; i < simState.width; i++) {
     for (let j = 0; j < simState.height; j++) {
-      // vary parabolically with respect to i
-      const wave = (Date.now() / 1000) % 10
-      const t = Math.pow(i - simState.width * wave + wave, 2) / Math.pow(simState.width * wave + wave, 2);
+      const wave = (((Date.now() / 1000) % 5) / 5) * simState.width;
+      const t = -(1/50000) * Math.pow((i - wave), 2) + 1;
 
       simState.dye[i][j] = Math.floor(t * 255);
     }
